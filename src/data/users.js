518638ffb2272e88ecb6b2d7e9309b9339e284bc
Hevertton Nunes de Oliveira <85510567+HeverttonNoliveira@users.noreply.github.com>
2024-05-07 08:21:12 -0300

    Primeiro Commit

---
+++ src/data/users.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id:1,
+        name: 'Kasandra',
+        email: 'Kasandra@mail.com',
+        avatarURL: 'https://cdn.pixabay.com/photo/2021/09/30/08/53/woman-6669461_1280.png',
+    },
+    {
+        id:2,
+        name: 'DogMeat',
+        email: 'DogMeat@mail.com',
+        avatarURL: 'https://cdn.pixabay.com/photo/2024/03/28/18/06/dog-8661433_1280.png',
+    },
+    {
+        id:3,
+        name: 'Iris',
+        email: 'Iris@mail.com',
+        avatarURL: 'https://cdn.pixabay.com/photo/2018/09/28/08/16/woman-3708719_960_720.png',
+    },
+    {
+        id:4,
+        name: 'Kess',
+        email: 'Kess@mail.com',
+        avatarURL: 'https://cdn.pixabay.com/photo/2022/02/28/07/28/cyberpunk-7038633_1280.png',
+    },
+    {
+        id:5,
+        name: 'Retro',
+        email: 'Retro@mail.com',
+        avatarURL: 'https://cdn.pixabay.com/photo/2023/09/07/14/08/ai-generated-8239171_960_720.png',
+    },
+
+]
---
+++ src/views/UserList.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, Text, FlatList, Alert } from "react-native";
+import users from "../data/users";
+import { ListItem, Avatar, ThemeProvider } from "@rneui/themed";
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default props => {
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir usuário', 'Deseja excluir o usuário ?',
+            [{
+                text: 'Sim',
+                onPress() { console.log('delete' + user.id) }
+            },
+            {
+                text: 'Não'
+            }
+            ])
+    }
+
+
+    function getUserIntem({ item: user }) {
+        return (
+            <ThemeProvider>
+                <ListItem
+                    bottomDivider
+                    onPress={() => { props.navigation.navigate('UserForm') }}
+                >
+                    <Avatar source={{ uri: user.avatarURL }} />
+                    <ListItem.Content>
+                        <ListItem.Title>{user.name}</ListItem.Title>
+                        <ListItem.Title>{user.email}</ListItem.Title>
+                    </ListItem.Content>
+
+                    <ListItem.Chevron
+                        name="edit"
+                        color="orange"
+                        size={25}
+                        onPress={() => props.navigation.navigate('UserForm', user)}
+                    />
+                    <ListItem.Chevron
+                        name="delete"
+                        color="red"
+                        size={25}
+                        onPress={() => confirmUserDeletion(user)}
+                    />
+                </ListItem>
+            </ThemeProvider>
+        )
+    }
+    return (
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserIntem}
+            />
+        </View>
+    )
+}
